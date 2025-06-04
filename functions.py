904b2ce8a0d8ef97230b7009b3f5a6356370665a
TrulsronDA <TrulsronDA>
2025-06-04 07:08:45 +0000

    Laget en funksjon for skriving og lagring til delta tabell, CSV-fil og sortert dataFrame etter "country", "indikator_code" og "year" i stiggende rekkefølge

--- functions.py
+++ functions.py
@@ -1,6 +1,8 @@
 import pyspark.sql.functions as sf
 import re
+import os
 from pyspark.sql import DataFrame
+from databricks.sdk.runtime import *
 
 def to_snake_case(column_name: str) -> str:
     """
@@ -42,3 +44,34 @@ def rename_columns_to_snake_case(df: DataFrame) -> DataFrame:
     """
     new_columns = [to_snake_case(col) for col in df.columns]
     return df.toDF(*new_columns)
+
+
+def save_df_as_delta_and_csv(
+    df: DataFrame,
+    base_path: str,
+    name: str,
+    mode: str
+):
+    """
+    Lagrer en Pyspark DataFrame som Delta og CSV med ordentlig struktur.
+    
+    Parameters:
+    - df (DataFrame): Pyspark Dataframe til lagring
+    - base_path (str): base directory path (f.eks. "/Volumes/dev_truls/default/data_files")
+    - name (str): navnet til datasettet (som skal brukes for directory og filnavn)
+    - mode (str): skriver mode, f.eks. "overwrite" or "append"
+    """
+
+    # Create Delta directory
+    delta_path = f"{base_path}/{name}"
+    df.write.format("delta").mode(mode).option("mergeSchema", "true").save(delta_path)
+    
+    # Convert to Pandas and create CSV string
+    pandas_df = df.toPandas()
+    csv_string = pandas_df.to_csv(index=False, header=True)
+    
+    # Define full CSV path
+    csv_path = f"{base_path}/{name}.csv"
+    
+    # Write CSV file to destination
+    dbutils.fs.put(csv_path, csv_string, True)
